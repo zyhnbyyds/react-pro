8d52c619ed716e794ab7e61955dd27ed8ca01bce
zhang771 <19939926438@163.com>
2023-04-18 23:05:51 +0800

    first commit

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,45 @@
+import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
+import { PageContainer } from '@ant-design/pro-components';
+import { useIntl } from '@umijs/max';
+import { Alert, Card, Typography } from 'antd';
+import React from 'react';
+
+const Admin: React.FC = () => {
+  const intl = useIntl();
+  return (
+    <PageContainer
+      content={intl.formatMessage({
+        id: 'pages.admin.subPage.title',
+        defaultMessage: 'This page can only be viewed by admin',
+      })}
+    >
+      <Card>
+        <Alert
+          message={intl.formatMessage({
+            id: 'pages.welcome.alertMessage',
+            defaultMessage: 'Faster and stronger heavy-duty components have been released.',
+          })}
+          type="info"
+          showIcon={false}
+          banner
+          style={{
+            margin: 0,
+            marginBottom: 80,
+          }}
+        />
+        <Typography.Title level={2} style={{ textAlign: 'center' }}>
+          <SmileTwoTone /> Ant Design Proasd <HeartTwoTone twoToneColor="#eb2f96" /> Youasddsa
+        </Typography.Title>
+      </Card>
+      <p style={{ textAlign: 'center', marginTop: 24 }}>
+        Want to add more pages? Please refer to{' '}
+        <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
+          use block
+        </a>
+        。
+      </p>
+    </PageContainer>
+  );
+};
+
+export default Admin;
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test: React.FC = () => {
+  return <div>你好</div>;
+};
+
+export default Test;
